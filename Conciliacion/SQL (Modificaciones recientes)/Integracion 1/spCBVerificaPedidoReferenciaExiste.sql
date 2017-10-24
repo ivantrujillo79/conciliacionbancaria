3e960fe60a5e360e72341da1c2268df84fd7fd34
ivantrujillo79 <ivan.trujillo.79@gmail.com>
2017-10-24 11:19:18 -0500

    Ajuste al procedimiento almacenado para validar que el pedido exista y además tenga Saldo mayor a Cero

--- Conciliacion/SQL (Modificaciones recientes)/Integracion 1/spCBVerificaPedidoReferenciaExiste.sql
+++ Conciliacion/SQL (Modificaciones recientes)/Integracion 1/spCBVerificaPedidoReferenciaExiste.sql
@@ -1,24 +1,21 @@
-USE [Sigamet]
+USE SIGAMET
 GO
 /********************************************************
 Realizó: Iván Trujillo
 Fecha: 5/10/2017
 Descripción: Verifica si un pedido referencia existe o no
  spCBVerificaPedidoReferenciaExiste 201781234567
-Actualizó: Ricardo Rojas - 23/10/2017
 *********************************************************/
 
-ALTER PROCEDURE [dbo].[spCBVerificaPedidoReferenciaExiste]
+CREATE PROCEDURE spCBVerificaPedidoReferenciaExiste
 @PedidoReferencia varchar(20)
 AS
-
 DECLARE @TotalRegistros INT;
-
 SET @TotalRegistros = 0;
 
 SELECT @TotalRegistros = COUNT(*)
 FROM Pedido 
-WHERE PedidoReferencia = @PedidoReferencia;
+WHERE PedidoReferencia = @PedidoReferencia and saldo > 0.01;
 
 SELECT @TotalRegistros as TotalRegistros
 
