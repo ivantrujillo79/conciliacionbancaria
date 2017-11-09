ee7bed424a46576ceccbae8ebfc0596b65d31afe
Diego J. C.L <dkayzva@softgrade.mx>
2017-11-09 12:33:15 -0600

    Cambios de consultar documentos

---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/Conciliacion/ConsultarDocumentos.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Conciliacion_ConsultarDocumentos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            ClaseFiltros filtros = new ClaseFiltros();
+            filtros = (ClaseFiltros)HttpContext.Current.Session["filtros"];
+            Label1.Text = "Empresa: " + filtros.Empresa+". Sucursal: "+filtros.Sucursal+". Grupo: " + filtros.Grupo+".";
+        }            
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
