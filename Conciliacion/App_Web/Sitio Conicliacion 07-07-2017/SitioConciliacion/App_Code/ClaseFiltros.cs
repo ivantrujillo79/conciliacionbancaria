1d60c385de96cf9129f058287534b17ba92a12dc
Diego J. C.L <dkayzva@softgrade.mx>
2017-11-09 12:42:03 -0600

    Se agrega clase

---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/App_Code/ClaseFiltros.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de ClaseFiltros
+/// </summary>
+public class ClaseFiltros
+{
+    
+    private int empresa;
+    public int Empresa
+    {
+        get
+        {
+            return empresa;
+        }
+        set
+        {
+            empresa = value;
+        }
+    }
+
+    private int sucursal;
+    public int Sucursal { get; set; }
+
+    private int grupo;
+    public int Grupo { get; set; }
+
+    private int tipoConciliacion;
+    public int TipoConciliacion { get; set; }
+
+    private int status;
+    public int Status { get; set; }
+
+    private int anio;
+    public int Anio { get; set; }
+
+    private int mes;
+    public int Mes { get; set; }
+
+    private int folio;
+    public int Folio { get; set; }
+
+    public ClaseFiltros()
+    {
+        empresa = -1;
+        sucursal = -1;
+        grupo = -1;
+        tipoConciliacion = -1;
+        status = -1;
+        anio = -1;
+        mes = -1;
+        folio = -1;
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
