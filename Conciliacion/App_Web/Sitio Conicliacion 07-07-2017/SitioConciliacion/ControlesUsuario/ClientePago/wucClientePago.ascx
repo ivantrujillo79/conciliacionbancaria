76be43206f401dbbe40f249b560c5cc01c6e959a
Ricardo Rojas <rrojas@transformait.com.mx>
2017-12-30 13:29:06 -0600

    seleccion de cliente: se selecciona el padre, si no hay padre se selecciona el hijo con el numero de cliente mas pequeno Remover duplidos Validad cliente Asignar nombre de cliente Ordenar por número de Cliente de forma ascendente Selecciona uno solamente (radiobutton)

--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/ControlesUsuario/ClientePago/wucClientePago.ascx
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/ControlesUsuario/ClientePago/wucClientePago.ascx
@@ -1,23 +1,39 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="wucClientePago.ascx.cs" Inherits="ControlesUsuario_ClientePago_wucClientePago" %>
 
 
-                        <asp:GridView ID="grvClientes" runat="server" ShowHeader="True"
-                            CssClass="grvResultadoConsultaCss" ShowFooter="False" Width="100%"
+
+<%--
+    El gridview deberá mostrar sólo cinco elementos y si el dataset tiene más elementos el grid deberá mostrar un scrollbar automáticamente
+    
+    --%>
+    <asp:HiddenField ID="hdfIndiceFila" runat="server" />
+
+                        <asp:GridView ID="grvClientes" runat="server" 
+                            ShowHeader="True"
+                            AllowSorting="True" 
+                            CssClass="grvResultadoConsultaCss" 
+                            ShowFooter="False" 
+                            Width="100%"
                             ShowHeaderWhenEmpty="True" 
-                            AllowPaging="False">
+                            AllowPaging="False"
+                            OnRowCommand="grvClientes_RowCommand" CommandName="SeleccionarCliente"
+                            OnRowDataBound="grvClientes_RowDataBound" >
+
                             <HeaderStyle HorizontalAlign="Center" />
                             <Columns>
                                 <asp:TemplateField>
                                     <ItemTemplate>
                                         <asp:RadioButton ID="RadioButton1" runat="server" 
+                                            AutoPostBack="true"
                                             ToolTip="SELECCIONAR CLIENTE"
+                                            OnCheckedChanged="RadioButton1_CheckedChanged"
                                             />
                                     </ItemTemplate>
                                     <ItemStyle HorizontalAlign="Center" Width="25px" BackColor="#ebecec"></ItemStyle>
                                     <HeaderStyle HorizontalAlign="Center" Width="25px"></HeaderStyle>
                                 </asp:TemplateField>
                                 
-                                <asp:TemplateField HeaderText="Cliente" SortExpression="Cliente">
+                                <%--<asp:TemplateField HeaderText="Cliente" SortExpression="Cliente">
                                     <ItemTemplate>
                                             <asp:Label ID="lblCliente" runat="server" Text='<%# Eval("Cliente") %>'></asp:Label>
                                     </ItemTemplate>
@@ -39,7 +55,7 @@
                                     </ItemTemplate>
                                     <ItemStyle HorizontalAlign="Center"></ItemStyle>
                                     <HeaderStyle HorizontalAlign="Center"></HeaderStyle>
-                                </asp:TemplateField>                                
+                                </asp:TemplateField>      --%>                          
                                 
                             </Columns>
                             <PagerStyle CssClass="grvPaginacionScroll" />
--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/ControlesUsuario/ClientePago/wucClientePago.ascx.cs
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/ControlesUsuario/ClientePago/wucClientePago.ascx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Conciliacion.RunTime.ReglasDeNegocio;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -43,9 +45,53 @@ public partial class ControlesUsuario_ClientePago_wucClientePago : System.Web.UI
         }
     }
 
+    DataTable dt = new DataTable();
+
+    private int indiceExternoSeleccionado
+    {
+        get { return Convert.ToInt32(hdfIndiceFila.Value); }
+        set { hdfIndiceFila.Value = value.ToString(); }
+    }
+
     private void CargarGrid()
     {
+        
+        //seleccion de cliente: se selecciona el padre, si no hay padre se selecciona el hijo con el numero de cliente mas pequeno
+        
+        //remover duplidos
+        Clientes = Clientes.Distinct().ToList();
+        
+        dt.Clear();
+        dt.Columns.Clear();
+        dt.Columns.Add("Cliente");
+        dt.Columns.Add("Nombre");
+        dt.Columns.Add("Tipo");
 
+        DataRow row;
+        foreach (string cli in Clientes)
+        {
+            //validar
+            Cliente objCliente = Conciliacion.RunTime.App.Cliente.CrearObjeto();
+            Conciliacion.RunTime.DatosSQL.Conexion conexion = new Conciliacion.RunTime.DatosSQL.Conexion();
+            conexion.AbrirConexion(true);
+            objCliente.Referencia = cli;
+            if (objCliente.ValidaClienteExiste(conexion))
+            {
+                //asignar nombre y tipo 
+                row = dt.NewRow();
+                row["Cliente"] = cli;
+                row["Nombre"] = objCliente.Nombre;
+                row["Tipo"] = "SUCURSAL"; //FALTA
+                dt.Rows.Add(row);
+            }
+        }
+
+        //El gridview deberá mostrar los registros ordenados por número de Cliente de forma ascendente 
+        dt.DefaultView.Sort = "Cliente";
+
+        grvClientes.DataSource = dt;
+        grvClientes.DataBind();
+        
     }
 
     private void ObtieneDetalleClientes()
@@ -55,6 +101,94 @@ public partial class ControlesUsuario_ClientePago_wucClientePago : System.Web.UI
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(!IsPostBack)
+        {
+            CargarGrid();
+        }
     }
+
+    protected void grvClientes_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        Response.Write(e.CommandArgument);
+        if (e.CommandName == "SeleccionarCliente")
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            // Recupera la fila en la que se hizo clic al botón
+            GridViewRow row = grvClientes.Rows[index];
+        }
+    }
+
+    public void pintarFilaSeleccionada(int fila)
+    {
+        grvClientes.Rows[fila].CssClass = "bg-color-azulClaro01 fg-color-blanco";
+        grvClientes.Rows[fila].Cells[0].CssClass = "bg-color-azulClaro01";
+        grvClientes.Rows[fila].Cells[1].CssClass = "bg-color-azulClaro01";
+        grvClientes.Rows[fila].Cells[2].CssClass = "bg-color-azulClaro01";
+    }
+
+    public void despintarFilaSeleccionada(int fila)
+    {
+        grvClientes.Rows[fila].CssClass = "bg-color-blanco fg-color-negro";
+        grvClientes.Rows[fila].Cells[0].CssClass = "bg-color-grisClaro03";
+        grvClientes.Rows[fila].Cells[1].CssClass = "bg-color-grisClaro03";
+        grvClientes.Rows[fila].Cells[2].CssClass = "bg-color-grisClaro03";
+    }
+
+    public void quitarSeleccionRadio()
+    {
+        foreach (
+            RadioButton rb in
+                from GridViewRow gv in grvClientes.Rows
+                select (RadioButton)grvClientes.Rows[gv.RowIndex].FindControl("RadioButton1"))
+        {
+            rb.Checked = false;
+            despintarFilaSeleccionada(((GridViewRow)rb.Parent.Parent).RowIndex);
+        }
+    }
+
+    protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            quitarSeleccionRadio();
+            RadioButton rdb = sender as RadioButton;
+            rdb.Checked = true;
+            GridViewRow grv = (GridViewRow)rdb.Parent.Parent;
+            pintarFilaSeleccionada(grv.RowIndex);
+
+            indiceExternoSeleccionado = grv.RowIndex;
+
+            int indexfila = 0;
+            GridViewRowCollection filas = grvClientes.Rows;
+            foreach (GridViewRow f in filas)
+            {
+                if (indiceExternoSeleccionado == indexfila) //f.RowIndex
+                    clienteseleccionado = f.Cells[1].Text; //break
+                indexfila = indexfila + 1;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "UpdateMsg",
+                @"alertify.alert('Conciliaci&oacute;n bancaria','Error: "
+                + ex.Message + "', function(){ alertify.error('Error en la solicitud'); });", true);
+        }
+    }
+
+
+    protected void grvClientes_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Footer)
+        {
+            if (grvClientes.Rows.Count > 0)
+            {
+                RadioButton rdb = grvClientes.Rows[0].FindControl("RadioButton1") as RadioButton;
+                rdb.Checked = true;
+                indiceExternoSeleccionado = 0;
+                pintarFilaSeleccionada(0);
+            }
+        }
+    }
+
 }
