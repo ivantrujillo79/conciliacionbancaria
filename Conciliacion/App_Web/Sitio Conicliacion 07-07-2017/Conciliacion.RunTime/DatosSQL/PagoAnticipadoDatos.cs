e145fdd6ed9649d06dceb024187677e6750ebc11
Ricardo Rojas <rrojas@transformait.com.mx>
2018-01-31 10:26:36 -0600

    Se agrega clase pagoanticipado

---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/DatosSQL/PagoAnticipadoDatos.cs
@@ -0,0 +1,146 @@
+﻿using Conciliacion.RunTime.ReglasDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Conciliacion.RunTime.DatosSQL
+{
+    public class PagoAnticipadoDatos : PagoAnticipado
+    {
+
+        public PagoAnticipadoDatos(IMensajesImplementacion implementadorMensajes)
+            : base(implementadorMensajes)
+        {
+        }
+        public PagoAnticipadoDatos(int cliente,
+            int corporativoconciliacion,
+            int sucursalconciliacion,
+            int añoconciliacion,
+            int mesconciliacion,
+            int folioconciliacion,
+            int Secuenciarelacion,
+            int corporativoexteno,
+            int sucursalexterno,
+            int añoexterno,
+            int folioexterno,
+            int secuenciaexterno,
+            decimal montoexterno,
+            int formaconciliacion,
+            int statusconciliacion,
+            int motivonoconciliado,
+            string comentarionoconciliado,
+            string usuario,
+            DateTime falta,
+            string descripcion,
+            string usuariostatusconcepto,
+            DateTime fstatusconcepto, IMensajesImplementacion implementadorMensajes)
+            : base(corporativoconciliacion,
+                    sucursalconciliacion,
+                    añoconciliacion,
+                    mesconciliacion,
+                    folioconciliacion,
+                    Secuenciarelacion,
+                    corporativoexteno,
+                    sucursalexterno,
+                    añoexterno,
+                    folioexterno,
+                    secuenciaexterno,
+                    montoexterno,
+                    formaconciliacion,
+                    statusconciliacion,
+                    motivonoconciliado,
+                    comentarionoconciliado,
+                    usuario,
+                    falta,
+                    descripcion,
+                    usuariostatusconcepto,
+                    fstatusconcepto, implementadorMensajes)
+        {
+        }
+
+        public override PagoAnticipado CrearObjeto()
+        {
+            return new PagoAnticipadoDatos(this.ImplementadorMensajes);
+        }
+
+        public override bool ValidarClientePagoAnticipado(Conexion _conexion, int NumeroCliente)
+        {
+            bool resultado = false;
+            try
+            {
+                _conexion.Comando.CommandType = CommandType.StoredProcedure;
+                _conexion.Comando.CommandText = "spCBValidaClientePagoAnticipado";
+                _conexion.Comando.Parameters.Clear();
+                _conexion.Comando.Parameters.Add(new SqlParameter("@NumeroCliente", System.Data.SqlDbType.SmallInt)).Value = NumeroCliente;
+
+                SqlDataReader reader = _conexion.Comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        resultado = Convert.ToInt16(reader["VALIDO"]) == 1;
+                        break;
+                    }
+                    reader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return resultado;
+        }
+
+        public override bool RegistraConciliacionReferencia(Conexion _conexion, int CorporativoConciliacion, int SucursalConciliacion, int AñoConciliacion, int MesConciliacion, int FolioConciliacion, int SecuenciaRelacion, int CorporativoExterno, int SucursalExterno, int AñoExterno, int FolioExterno, int SecuenciaExterno, decimal MontoExterno, int FormaConciliacion, int StatusConcepto, string StatusConciliacion, int MotivoNoConciliado, string ComentarioNoConciliado, string Usuario, DateTime FAlta, string Descripcion, string UsuarioStatusConcepto, DateTime FStatusConcepto)
+        {
+            bool resultado = false;
+            StatusConcepto = 28;
+            StatusConciliacion = "CONCILIACION CANCELADA";
+            MotivoNoConciliado = 1;
+            try
+            {
+                _conexion.Comando.CommandType = CommandType.StoredProcedure;
+                _conexion.Comando.CommandText = "spCBAltaConciliacionReferencia";
+                _conexion.Comando.Parameters.Clear();
+                _conexion.Comando.Parameters.Add(new SqlParameter("@CorporativoConciliacion", System.Data.SqlDbType.SmallInt)).Value = CorporativoConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@SucursalConciliacion", System.Data.SqlDbType.SmallInt)).Value = SucursalConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@AñoConciliacion", System.Data.SqlDbType.Int)).Value = AñoConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@MesConciliacion", System.Data.SqlDbType.SmallInt)).Value = MesConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@FolioConciliacion", System.Data.SqlDbType.Int)).Value = FolioConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@SecuenciaRelacion", System.Data.SqlDbType.Int)).Value = SecuenciaRelacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@CorporativoExterno", System.Data.SqlDbType.SmallInt)).Value = CorporativoExteno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@SucursalExterno", System.Data.SqlDbType.SmallInt)).Value = SucursalExterno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@AñoExterno", System.Data.SqlDbType.Int)).Value = AñoExterno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@FolioExterno", System.Data.SqlDbType.Int)).Value = FolioExterno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@SecuenciaExterno", System.Data.SqlDbType.Int)).Value = SecuenciaExterno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@MontoExterno", System.Data.SqlDbType.Decimal)).Value = MontoExterno;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@FormaConciliacion", System.Data.SqlDbType.SmallInt)).Value = FormaConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@StatusConcepto", System.Data.SqlDbType.SmallInt)).Value = 28;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@StatusConciliacion", System.Data.SqlDbType.VarChar)).Value = StatusConciliacion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@MotivoNoConciliado", System.Data.SqlDbType.SmallInt)).Value = MotivoNoConciliado;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@ComentarioNoConciliado", System.Data.SqlDbType.VarChar)).Value = ComentarioNoConciliado;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@Usuario", System.Data.SqlDbType.Char)).Value = Usuario;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@FAlta", System.Data.SqlDbType.DateTime)).Value = FAlta;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@Descripcion", System.Data.SqlDbType.VarChar)).Value = Descripcion;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@UsuarioStatusConcepto", System.Data.SqlDbType.Char)).Value = UsuarioStatusConcepto;
+                _conexion.Comando.Parameters.Add(new SqlParameter("@FStatusConcepto", System.Data.SqlDbType.DateTime)).Value = FStatusConcepto;
+
+                int RegistrosAfectados = _conexion.Comando.ExecuteNonQuery();
+                resultado = RegistrosAfectados == 0;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return resultado;
+        }
+
+
+    }
+}
---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/ReglasDeNegocio/PagoAnticipado.cs
@@ -0,0 +1,218 @@
+﻿using Conciliacion.RunTime.DatosSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conciliacion.RunTime.ReglasDeNegocio
+{
+    public abstract class PagoAnticipado : EmisorMensajes
+    {
+        private int corporativoconciliacion;
+        private int sucursalconciliacion;
+        private int añoconciliacion;
+        private int mesconciliacion;
+        private int folioconciliacion;
+        private int secuenciarelacion;
+        private int corporativoexteno;
+        private int sucursalexterno;
+        private int añoexterno;
+        private int folioexterno;
+        private int secuenciaexterno;
+        private decimal montoexterno;
+        private int formaconciliacion;
+        private int statusconciliacion;
+        private int motivonoconciliado;
+        private string comentarionoconciliado;
+        private string usuario;
+        private DateTime falta;
+        private string descripcion;
+        private string usuariostatusconcepto;
+        private DateTime fstatusconcepto;
+
+        public PagoAnticipado(IMensajesImplementacion implementadorMensajes)
+        {
+            this.implementadorMensajes = implementadorMensajes;
+            this.corporativoconciliacion = 0;
+            this.sucursalconciliacion = 0;
+            this.añoconciliacion = 0;
+            this.mesconciliacion = 0;
+            this.folioconciliacion = 0;
+            this.secuenciarelacion = 0;
+            this.corporativoexteno = 0;
+            this.sucursalexterno = 0;
+            this.añoexterno = 0;
+            this.folioexterno = 0;
+            this.secuenciaexterno = 0;
+            this.montoexterno = 0;
+            this.formaconciliacion = 0;
+            this.statusconciliacion = 0;
+            this.motivonoconciliado = 0;
+            this.comentarionoconciliado = "";
+            this.usuario = "";
+            this.falta = DateTime.MinValue;
+            this.descripcion = "";
+            this.usuariostatusconcepto = "";
+            this.fstatusconcepto = DateTime.MinValue;
+        }
+
+        public PagoAnticipado(
+            int corporativoconciliacion,
+            int sucursalconciliacion,
+            int añoconciliacion,
+            int mesconciliacion,
+            int folioconciliacion,
+            int Secuenciarelacion,
+            int corporativoexteno,
+            int sucursalexterno,
+            int añoexterno,
+            int folioexterno,
+            int secuenciaexterno,
+            decimal montoexterno,
+            int formaconciliacion,
+            int statusconciliacion,
+            int motivonoconciliado,
+            string comentarionoconciliado,
+            string usuario,
+            DateTime falta,
+            string descripcion,
+            string usuariostatusconcepto,
+            DateTime fstatusconcepto,
+            IMensajesImplementacion implementadorMensajes)
+        {
+            this.corporativoconciliacion = 0;
+            this.sucursalconciliacion = 0;
+            this.añoconciliacion = 0;
+            this.mesconciliacion = 0;
+            this.folioconciliacion = 0;
+            this.secuenciarelacion = 0;
+            this.corporativoexteno = 0;
+            this.sucursalexterno = 0;
+            this.añoexterno = 0;
+            this.folioexterno = 0;
+            this.secuenciaexterno = 0;
+            this.montoexterno = 0;
+            this.formaconciliacion = 0;
+            this.statusconciliacion = 0;
+            this.motivonoconciliado = 0;
+            this.comentarionoconciliado = "";
+            this.usuario = "";
+            this.falta = DateTime.MinValue;
+            this.descripcion = "";
+            this.usuariostatusconcepto = "";
+            this.fstatusconcepto = DateTime.MinValue;
+            this.implementadorMensajes = implementadorMensajes;
+        }
+
+        public int CorporativoConciliacion
+        {
+            get { return corporativoconciliacion; }
+            set { corporativoconciliacion = value; }
+        }
+        public int SucursalConciliacion
+        {
+            get { return sucursalconciliacion; }
+            set { sucursalconciliacion = value; }
+        }
+        public int AñoConciliacion
+        {
+            get { return añoconciliacion; }
+            set { añoconciliacion = value; }
+        }
+        public int MesConciliacion
+        {
+            get { return mesconciliacion; }
+            set { mesconciliacion = value; }
+        }
+        public int FolioConciliacion
+        {
+            get { return folioconciliacion; }
+            set { folioconciliacion = value; }
+        }
+        public int SecuenciaRelacion
+        {
+            get { return secuenciarelacion; }
+            set { secuenciarelacion = value; }
+        }
+        public int CorporativoExteno
+        {
+            get { return corporativoexteno; }
+            set { corporativoexteno = value; }
+        }
+        public int SucursalExterno
+        {
+            get { return sucursalexterno; }
+            set { sucursalexterno = value; }
+        }
+        public int AñoExterno
+        {
+            get { return añoexterno; }
+            set { añoexterno = value; }
+        }
+        public int FolioExterno
+        {
+            get { return folioexterno; }
+            set { folioexterno = value; }
+        }
+        public int SecuenciaExterno
+        {
+            get { return secuenciaexterno; }
+            set { secuenciaexterno = value; }
+        }
+        public decimal MontoExterno
+        {
+            get { return montoexterno; }
+            set { montoexterno = value; }
+        }
+        public int FormaConciliacion
+        {
+            get { return formaconciliacion; }
+            set { formaconciliacion = value; }
+        }
+        public int StatusConciliacion
+        {
+            get { return statusconciliacion; }
+            set { statusconciliacion = value; }
+        }
+        public int MotivoNoConciliado
+        {
+            get { return motivonoconciliado; }
+            set { motivonoconciliado = value; }
+        }
+        public string ComentarioNoConciliado
+        {
+            get { return comentarionoconciliado; }
+            set { comentarionoconciliado = value; }
+        }
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public DateTime FAlta
+        {
+            get { return falta; }
+            set { falta = value; }
+        }
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+        public string UsuarioStatusConcepto
+        {
+            get { return usuariostatusconcepto; }
+            set { usuariostatusconcepto = value; }
+        }
+        public DateTime FStatusConcepto
+        {
+            get { return fstatusconcepto; }
+            set { fstatusconcepto = value; }
+        }
+
+        public abstract PagoAnticipado CrearObjeto();
+        public abstract bool ValidarClientePagoAnticipado(Conexion _conexion, int NumeroCliente);
+        public abstract bool RegistraConciliacionReferencia(Conexion _conexion, int CorporativoConciliacion, int SucursalConciliacion, int AñoConciliacion, int MesConciliacion, int FolioConciliacion, int SecuenciaRelacion, int CorporativoExterno, int SucursalExterno, int AñoExterno, int FolioExterno, int SecuenciaExterno, decimal MontoExterno, int FormaConciliacion, int StatusConcepto, string StatusConciliacion, int MotivoNoConciliado, string ComentarioNoConciliado, string Usuario, DateTime FAlta, string Descripcion, string UsuarioStatusConcepto, DateTime FStatusConcepto);
+
+    }
+}
