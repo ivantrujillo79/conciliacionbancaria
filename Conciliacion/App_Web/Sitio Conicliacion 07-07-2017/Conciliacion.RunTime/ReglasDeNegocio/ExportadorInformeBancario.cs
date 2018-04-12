a7e632024a6d5e15c7577d0cf188bf630b85198a
Ruben Morera <rubencho_01@hotmail.com>
2018-04-12 17:27:53 -0500

    Lista la funcionalidad de los métodos exportarPosicionDiariaBancos() y calcularTotalizadores()

--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/ReglasDeNegocio/ExportadorInformeBancario.cs
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/ReglasDeNegocio/ExportadorInformeBancario.cs
@@ -22,11 +22,22 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
         private Microsoft.Office.Interop.Excel.Worksheet xlHoja;
         private Microsoft.Office.Interop.Excel.Range xlRango;
 
-        private string _Nombre;
         private List<DetallePosicionDiariaBancos> _DetallePosicionDiariaBancos;
         private string _Ruta;
         private string _Archivo;
         private string _NombreLibro;
+
+        private const string CONCEPTO1 = "PORTATIL";
+        private const string CONCEPTO2 = "ESTACIONARIO";
+        private const string CONCEPTO3 = "EDIFICIOS";
+        private const string CONCEPTO4 = "SERVICIOS TECNICOS";
+        private const string CONCEPTO5 = "CREDITO PORTATIL";
+        private const string CONCEPTO6 = "CREDITO ESTACIONARIO";
+        private const string CONCEPTO7 = "CREDITO EDIFICIOS";
+        private const string CONCEPTO8 = "CREDITO SERVICIOS TECNICOS";
+        private const string CONCEPTO9 = "COBRANZA";
+        private const string CONCEPTO10 = "COBRANZA FILIAL";
+        private const string CONCEPTO11 = "OTROS INGRESOS";
 
         #endregion
 
@@ -35,7 +46,6 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
         public ExportadorInformeBancario(List<DetallePosicionDiariaBancos> DetallePosicionDiariaBancos, 
                                         string Ruta, string Archivo, string Nombre)
         {
-            _Nombre = Nombre;
             _DetallePosicionDiariaBancos = DetallePosicionDiariaBancos;
             _Ruta = Ruta.Trim();
             _Archivo = Archivo.Trim();
@@ -43,8 +53,7 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
 
             //ValidarParametrosConstructor();
         }
-
-
+        
         #endregion
 
         public void generarPosicionDiariaBancos()
@@ -53,6 +62,8 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
             {
                 inicializar();
                 crearEncabezado();
+                exportarPosicionDiariaBancos();
+                calcularTotalizadores();
                 cerrar();
             }
             catch (Exception ex)
@@ -97,12 +108,10 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
 
         private void crearEncabezado()
         {
-            //Excel.Range xlRango;
-            DateTime fecha1;
-            DateTime fecha2;
-            string dia1;
-            string dia2;
             CultureInfo cultureInfo = CultureInfo.GetCultureInfo("es-MX");
+            DateTime fecha = _DetallePosicionDiariaBancos[0].Fecha;
+            string dia = fecha.ToString("dddd", cultureInfo).ToUpper();
+            string caja = Convert.ToString(_DetallePosicionDiariaBancos[0].Caja);
 
             // Nombre del reporte
             xlRango = xlHoja.Range["A1"];
@@ -113,48 +122,159 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
             xlRango.RowHeight = 15;
             xlRango.Borders.LineStyle = Excel.XlLineStyle.xlDouble;
             xlRango.Interior.Color = Excel.XlRgbColor.rgbSkyBlue;
-            // Día de la semana
-            fecha1 = DateTime.Now;
-            fecha2 = DateTime.Now.AddDays(1);
-            dia1 = fecha1.ToString("dddd", cultureInfo).ToUpper();
-            dia2 = fecha2.AddDays(1).ToString("dddd", cultureInfo).ToUpper();
+            // Día
             xlRango = xlHoja.Range["F1:G1"];
             xlRango.Merge();
-            xlRango.Value2 = dia1;
-            // Día 2
-            xlRango = xlHoja.Range["H1:I1"];
-            xlRango.Merge();
-            xlRango.Value2 = dia2;
+            xlRango.Value2 = dia;
             // Kilos
-            xlRango = xlHoja.Range["F2,H2"];
-            xlRango.Value2 = "KILOS";
-            //Fecha
-            xlRango = xlHoja.Range["G2,I2"];
-            xlRango[1].Value2 = fecha1.ToString("d-MMM-yyyy", cultureInfo);
-            xlRango[1,3].Value2 = fecha2.ToString("d-MMM-yyyy", cultureInfo);
+            xlRango = xlHoja.Range["F2,G2"];
+            xlRango[1, 1].Value2 = "KILOS";
+            // Fecha
+            xlRango[1, 2].Value2 = fecha.ToString("d-MMM-yyyy", cultureInfo);
             // Formato
-            xlRango = xlHoja.Range["F1:I2"];
+            xlRango = xlHoja.Range["F1:G2"];
             xlRango.Borders.LineStyle = Excel.XlLineStyle.xlDouble;
             xlRango.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
-            xlRango.ColumnWidth = 15;
-            
-            xlRango = xlHoja.Range["A3:E200"];
-            xlRango.Merge(true);
+            xlRango.ColumnWidth = 16;
+            // Caja
+            xlRango = xlHoja.Range["A3:E3"];
+            xlRango.Merge();
+            xlRango.Value2 = "CAJA MATRIZ " + caja;
+            xlRango.Borders.LineStyle = Excel.XlLineStyle.xlDouble;
+            xlRango.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
         }
 
         private void exportarPosicionDiariaBancos()
         {
-            string celda;
-            int contador = 4;
+            string concepto;
 
-            foreach(DetallePosicionDiariaBancos item in _DetallePosicionDiariaBancos)
+            xlRango = xlHoja.Range["A4:E19"];
+            xlRango.Merge(true);
+            xlRango[1, 1].Value2 = "Portátil";
+            xlRango[2, 1].Value2 = "Estacionario";
+            xlRango[3, 1].Value2 = "Edificios";
+            xlRango[4, 1].Value2 = "Servicios técnicos";
+            xlRango[5, 1].Value2 = "Crédito portátil";
+            xlRango[6, 1].Value2 = "Crédito estacionario";
+            xlRango[7, 1].Value2 = "Crédito edificios";
+            xlRango[8, 1].Value2 = "Crédito servicios técnicos";
+            xlRango[9, 1].Value2 = "Cobranza";
+            xlRango[10, 1].Value2 = "Cobranza filial";
+            xlRango[11, 1].Value2 = "Otros ingresos";
+
+            xlRango[13, 1].Value2 = "Total ingresos del día";
+            xlRango[14, 1].Value2 = "Total crédito";
+            xlRango[16, 1].Value2 = "Total ingresos del día neto";
+            xlRango = xlHoja.Range["A16:A19"];
+            xlRango.Font.Bold = true;
+
+            // Seleccionar cuadro de celdas donde se imprimirán los datos
+            xlRango = xlHoja.Range["F4:G14"];
+            foreach (DetallePosicionDiariaBancos item in _DetallePosicionDiariaBancos)
             {
-                celda = "A" + contador;
-                xlRango = xlHoja.Range[celda];
-                xlRango.Value2 = item.ToString();
-                ++contador;
-                item.Año.ToString();
+                concepto = RemoverAcentos(item.Concepto.ToUpper().Trim());
+
+                switch (concepto)
+                {
+                    case CONCEPTO1:
+                        xlRango[1, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[1, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO2:
+                        xlRango[2, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[2, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO3:
+                        xlRango[3, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[3, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO4:
+                        xlRango[4, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[4, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO5:
+                        xlRango[5, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[5, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO6:
+                        xlRango[6, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[6, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO7:
+                        xlRango[7, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[7, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO8:
+                        xlRango[8, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[8, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO9:
+                        xlRango[9, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[9, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO10:
+                        xlRango[10, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[10, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    case CONCEPTO11:
+                        xlRango[11, 1].Value2 = item.Kilos.ToString("0,0.##");
+                        xlRango[11, 2].Value2 = item.Importe.ToString("C");
+                        break;
+                    default:
+                        break;
+                }
             }
+        }
+
+        private void calcularTotalizadores()
+        {
+            string concepto;
+            decimal totalIngresoDia = 0m;
+            decimal totalCredito = 0m;
+            decimal totalNeto = 0m;
+            decimal totalIngresoDiaKilos = 0m;
+            decimal totalCreditoKilos = 0m;
+            decimal totalNetoKilos = 0m;
+
+            foreach (DetallePosicionDiariaBancos item in _DetallePosicionDiariaBancos)
+            {
+                concepto = RemoverAcentos(item.Concepto.Substring(0, 5).ToUpper().Trim());
+
+                if ( (!concepto.Equals("CREDI")) && (!concepto.Equals("TOTAL")) )
+                {
+                    totalIngresoDia += item.Importe;
+                    totalIngresoDiaKilos += item.Kilos;
+                }
+                else if (concepto.Equals("CREDI"))
+                {
+                    totalCredito += item.Importe;
+                    totalCreditoKilos += item.Kilos;
+                }
+            }
+
+            totalNeto = totalIngresoDia + totalCredito;
+            totalNetoKilos = totalIngresoDiaKilos + totalCreditoKilos;
+
+            xlRango = xlHoja.Range["F16:G19"];
+            // Total kilos
+            xlRango[1, 1].Value2 = totalIngresoDiaKilos.ToString("0,0.##");
+            xlRango[2, 1].Value2 = totalCreditoKilos.ToString("0,0.##");
+            xlRango[3, 1].Value2 = "-   ";
+            xlRango[3, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+            xlRango[4, 1].Value2 = totalNetoKilos.ToString("0,0.##");
+            // Total importe
+            xlRango[1, 2].Value2 = totalIngresoDia.ToString("C");
+            xlRango[2, 2].Value2 = totalCredito.ToString("C");
+            xlRango[3, 2].Value2 = "-   ";
+            xlRango[3, 2].HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+            xlRango[4, 2].Value2 = totalNeto.ToString("C");
+
+            // Formato
+            xlRango = xlHoja.Range["A16:G19"];
+            xlRango.Interior.Color = Excel.XlRgbColor.rgbSkyBlue;
+            xlRango = xlHoja.Range["A1:G19"];
+            xlRango.BorderAround2(Excel.XlLineStyle.xlDouble, Excel.XlBorderWeight.xlThin, 
+                Excel.XlColorIndex.xlColorIndexAutomatic);
         }
 
         private void cerrar()
@@ -167,7 +287,6 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
                             false, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing,
                             Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
-            //xlLibro.Close(true, _Archivo, Type.Missing);
             xlLibro.Close(0);
             xlLibros.Close();
             xlAplicacion.Quit();
@@ -213,5 +332,14 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
 
             return valido;
         } // end ValidarParametrosConstructor()
+        
+        private string RemoverAcentos(string text)
+        {
+            return string.Concat(
+                text.Normalize(NormalizationForm.FormD)
+                .Where(ch => CharUnicodeInfo.GetUnicodeCategory(ch) !=
+                                              UnicodeCategory.NonSpacingMark)
+              ).Normalize(NormalizationForm.FormC);
+        }
     }
 }
