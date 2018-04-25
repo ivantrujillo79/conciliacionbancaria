0448c8d3e8afa77909ad0421afd4a58ec85adb2a
Ruben Morera <rubencho_01@hotmail.com>
2018-04-25 16:39:38 -0500

    Se ajustan los tipos de dato en la clase DetalleReporteEstadoCuenta para que concuerden con las columnas en la base de datos

--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/DatosSQL/InformeBancarioDatos.cs
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/DatosSQL/InformeBancarioDatos.cs
@@ -122,151 +122,21 @@ namespace Conciliacion.RunTime.DatosSQL
             return new InformeBancarioDatos(this.ImplementadorMensajes);
         }
 
-        //public class DetallePosicionDiariaBancos
-        //{
-        //    private string corporativo;
-        //    private string sucursal;
-        //    private int año;
-        //    private int mes;
-        //    private string cuentabancofinanciero;
-        //    private int consecutivoflujo;
-        //    private DateTime fecha;
-        //    private string referencia;
-        //    private string concepto;
-        //    private decimal retiros;
-        //    private decimal depositos;
-        //    private decimal saldofinal;
-        //    private string conceptoconciliado;
-        //    private string documentoconciliado;
-
-        //    public string Corporativo
-        //    {
-        //        get { return corporativo; }
-        //        set { corporativo = value; }
-        //    }
-        //    public string Sucursal
-        //    {
-        //        get { return sucursal; }
-        //        set { sucursal = value; }
-        //    }
-        //    public int Año
-        //    {
-        //        get { return año; }
-        //        set { año = value; }
-        //    }
-        //    public int Mes
-        //    {
-        //        get { return mes; }
-        //        set { mes = value; }
-        //    }
-        //    public string CuentaBancoFinanciero
-        //    {
-        //        get { return cuentabancofinanciero; }
-        //        set { cuentabancofinanciero = value; }
-        //    }
-        //    public int ConsecutivoFlujo
-        //    {
-        //        get { return consecutivoflujo; }
-        //        set { consecutivoflujo = value; }
-        //    }
-        //    public DateTime Fecha
-        //    {
-        //        get { return fecha; }
-        //        set { fecha = value; }
-        //    }
-        //    public string Referencia
-        //    {
-        //        get { return referencia; }
-        //        set { referencia = value; }
-        //    }
-        //    public string Concepto
-        //    {
-        //        get { return concepto; }
-        //        set { concepto = value; }
-        //    }
-        //    public decimal Retiros
-        //    {
-        //        get { return retiros; }
-        //        set { retiros = value; }
-        //    }
-        //    public decimal Depositos
-        //    {
-        //        get { return depositos; }
-        //        set { depositos = value; }
-        //    }
-        //    public decimal SaldoFinal
-        //    {
-        //        get { return saldofinal; }
-        //        set { saldofinal = value; }
-        //    }
-        //    public string ConceptoConciliado
-        //    {
-        //        get { return conceptoconciliado; }
-        //        set { conceptoconciliado = value; }
-        //    }
-        //    public string DocumentoConciliado
-        //    {
-        //        get { return documentoconciliado; }
-        //        set { documentoconciliado = value; }
-        //    }
-
-        //    public DetallePosicionDiariaBancos()
-        //    {
-
-        //    }
-
-        //    public DetallePosicionDiariaBancos(string corporativo,
-        //                string sucursal,
-        //                int año,
-        //                int mes,
-        //                string cuentabancofinanciero,
-        //                int consecutivoflujo,
-        //                DateTime fecha,
-        //                string referencia,
-        //                string concepto,
-        //                decimal retiros,
-        //                decimal depositos,
-        //                decimal saldofinal,
-        //                string conceptoconciliado,
-        //                string documentoconciliado)
-        //    {
-        //        this.corporativo = corporativo;
-        //        this.sucursal = sucursal;
-        //        this.año = año;
-        //        this.mes = mes;
-        //        this.cuentabancofinanciero = cuentabancofinanciero;
-        //        this.consecutivoflujo = consecutivoflujo;
-        //        this.fecha = fecha;
-        //        this.referencia = referencia;
-        //        this.concepto = concepto;
-        //        this.retiros = retiros;
-        //        this.depositos = depositos;
-        //        this.saldofinal = saldofinal;
-        //        this.conceptoconciliado = conceptoconciliado;
-        //        this.documentoconciliado = documentoconciliado;
-        //    }
-
-        //    //public DetallePosicionDiariaBancos CrearObjeto()
-        //    //{
-        //    //return new DetallePosicionDiariaBancos(this.ImplementadorMensajes);
-        //    //}
-        //}
-
         public class DetalleReporteEstadoCuenta
         {
             private string banco;
             private string corporativo;
             private string sucursal;
-            private string año;
-            private string mes;
+            private short año;
+            private short mes;
             private string cuentabancofinanciero;
-            private string consecutivoflujo;
-            private string foperacion;
+            private int consecutivoflujo;
+            private DateTime foperacion;
             private string referencia;
             private string concepto;
-            private string retiros;
-            private string depositos;
-            private string saldofinal;
+            private decimal retiros;
+            private decimal depositos;
+            private decimal saldofinal;
 
             public string Banco
             {
@@ -283,12 +153,12 @@ namespace Conciliacion.RunTime.DatosSQL
                 get { return sucursal; }
                 set { sucursal = value; }
             }
-            public string Año
+            public short Año
             {
                 get { return año; }
                 set { año = value; }
             }
-            public string Mes
+            public short Mes
             {
                 get { return mes; }
                 set { mes = value; }
@@ -298,12 +168,12 @@ namespace Conciliacion.RunTime.DatosSQL
                 get { return cuentabancofinanciero; }
                 set { cuentabancofinanciero = value; }
             }
-            public string ConsecutivoFlujo
+            public int ConsecutivoFlujo
             {
                 get { return consecutivoflujo; }
                 set { consecutivoflujo = value; }
             }
-            public string FOpercion
+            public DateTime FOperacion
             {
                 get { return foperacion; }
                 set { foperacion = value; }
@@ -318,17 +188,17 @@ namespace Conciliacion.RunTime.DatosSQL
                 get { return concepto; }
                 set { concepto = value; }
             }
-            public string Retiros
+            public decimal Retiros
             {
                 get { return retiros; }
                 set { retiros = value; }
             }
-            public string Depositos
+            public decimal Depositos
             {
                 get { return depositos; }
                 set { depositos = value; }
             }
-            public string SaldoFinal
+            public decimal SaldoFinal
             {
                 get { return saldofinal; }
                 set { saldofinal = value; }
@@ -342,16 +212,16 @@ namespace Conciliacion.RunTime.DatosSQL
                 string banco,
                 string corporativo,
                 string sucursal,
-                string año,
-                string mes,
+                short año,
+                short mes,
                 string cuentabancofinanciero,
-                string consecutivoflujo,
-                string foperacion,
+                int consecutivoflujo,
+                DateTime foperacion,
                 string referencia,
                 string concepto,
-                string retiros,
-                string depositos,
-                string saldofinal)
+                decimal retiros,
+                decimal depositos,
+                decimal saldofinal)
             {
                 this.banco = banco;
                 this.corporativo = corporativo;
@@ -379,26 +249,26 @@ namespace Conciliacion.RunTime.DatosSQL
                     _conexion.Comando.Parameters.Add(new SqlParameter("@FechaIni", System.Data.SqlDbType.DateTime)).Value = FechaIni;
                     _conexion.Comando.Parameters.Add(new SqlParameter("@FechaFin", System.Data.SqlDbType.DateTime)).Value = FechaFin;
                     _conexion.Comando.Parameters.Add(new SqlParameter("@Banco", System.Data.SqlDbType.VarChar)).Value = Banco;
-                    _conexion.Comando.Parameters.Add(new SqlParameter("@CuentaBanco", System.Data.SqlDbType.VarChar)).Value = CuentaBanco;
+                    //_conexion.Comando.Parameters.Add(new SqlParameter("@CuentaBanco", System.Data.SqlDbType.VarChar)).Value = CuentaBanco;
                     SqlDataReader reader = _conexion.Comando.ExecuteReader();
                     if (reader.HasRows)
                     {
                         while (reader.Read())
                         {
                             DetalleReporteEstadoCuenta dato = new DetalleReporteEstadoCuenta();
-                            dato.Banco = (reader["banco"] == DBNull.Value ? "" : Convert.ToString(reader["banco"]));
+                            //dato.Banco = (reader["banco"] == DBNull.Value ? "" : Convert.ToString(reader["banco"]));
                             dato.Corporativo = Convert.ToString(reader["corporativo"]);
                             dato.Sucursal = Convert.ToString(reader["sucursal"]);
-                            dato.Año = Convert.ToString(reader["año"]);
-                            dato.Mes = Convert.ToString(reader["mes"]);
+                            dato.Año = Convert.ToInt16(reader["año"]);
+                            dato.Mes = Convert.ToInt16(reader["mes"]);
                             dato.CuentaBancoFinanciero = Convert.ToString(reader["cuentabancofinanciero"]);
-                            dato.ConsecutivoFlujo = Convert.ToString(reader["consecutivoflujo"]);
-                            dato.FOpercion = Convert.ToString(reader["foperacion"]);
+                            dato.ConsecutivoFlujo = Convert.ToInt32(reader["consecutivoflujo"]);
+                            dato.FOperacion = Convert.ToDateTime(reader["foperacion"]);
                             dato.Referencia = Convert.ToString(reader["referencia"]);
                             dato.Concepto = Convert.ToString(reader["concepto"]);
-                            dato.Retiros = Convert.ToString(reader["retiros"]);
-                            dato.Depositos = Convert.ToString(reader["depositos"]);
-                            dato.SaldoFinal = Convert.ToString(reader["saldofinal"]);
+                            dato.Retiros = Convert.ToDecimal(reader["retiros"]);
+                            dato.Depositos = Convert.ToDecimal(reader["depositos"]);
+                            dato.SaldoFinal = Convert.ToDecimal(reader["saldofinal"]);
                             ListaResultado.Add(dato);
                         }
                         reader.Close();
