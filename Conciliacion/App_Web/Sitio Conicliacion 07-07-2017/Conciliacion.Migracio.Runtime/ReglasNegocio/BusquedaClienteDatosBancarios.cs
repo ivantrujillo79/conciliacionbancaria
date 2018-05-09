82471204ba40ad7e9adec17d91b3799a79339f18
Ricardo Rojas <rrojas@transformait.com.mx>
2018-05-09 12:49:55 -0500

    Se crea clase BusquedaClienteDatosBancarios con metodo ConsultarCliente para buscar por cuenta bancaria, clabe, RFC o referencia de pago.

---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.Migracio.Runtime/ReglasNegocio/BusquedaClienteDatosBancarios.cs
@@ -0,0 +1,38 @@
+﻿using Conciliacion.Migracion.Runtime.SqlDatos;
+using Conciliacion.RunTime.DatosSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conciliacion.Migracion.Runtime.ReglasNegocio
+{
+    public abstract class BusquedaClienteDatosBancarios : EmisorMensajes
+    {
+        List<int> clientes;
+
+        #region constructores
+        public BusquedaClienteDatosBancarios(IMensajesImplementacion implementadorMensajes)
+        {
+            this.implementadorMensajes = implementadorMensajes;
+            this.clientes = new List<int>();
+        }
+
+        public BusquedaClienteDatosBancarios(List<int> Clientes, IMensajesImplementacion implementadorMensajes)
+        {
+            this.clientes = Clientes;
+            this.implementadorMensajes = implementadorMensajes;
+        }
+        #endregion
+
+        public List<int> Clientes
+        {
+            get { return clientes; }
+            set { clientes = value; }
+        }
+
+        public abstract BusquedaClienteDatosBancarios CrearObjeto();
+        public abstract List<int> ConsultarCliente(Int16 BuscarPor, string Dato);
+
+    }
+}
---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.Migracio.Runtime/SqlDatos/BusquedaClienteDatosBancariosDatos.cs
@@ -0,0 +1,67 @@
+﻿using Conciliacion.Migracion.Runtime.ReglasNegocio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Conciliacion.Migracion.Runtime.SqlDatos
+{
+    class BusquedaClienteDatosBancariosDatos : BusquedaClienteDatosBancarios
+    {
+        public override List<int> ConsultarCliente(short BuscarPor, string Dato)
+        {
+            //(1, "Cuenta bancaria");
+            //(2, "Clabe bancaria");
+            //(3, "RFC");
+            //(4, "Referencia de pago");
+
+            List<int> Clientes = new List<int>();
+            using (SqlConnection cnn = new SqlConnection(App.CadenaConexion))
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand comando = new SqlCommand("spCBConsultarCliente", cnn);
+                    comando.Parameters.Add("@BuscarPor", System.Data.SqlDbType.SmallInt).Value = BuscarPor;
+                    comando.Parameters.Add("@Dato", System.Data.SqlDbType.VarChar).Value = Dato;
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Clientes.Add(Convert.ToInt32(reader["Cliente"]));
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    stackTrace = new StackTrace();
+                    this.ImplementadorMensajes.MostrarMensaje("Error al consultar la información.\n\rClase :" +
+                                                              this.GetType().Name + "\n\r" + "Metodo :" +
+                                                              stackTrace.GetFrame(0).GetMethod().Name + "\n\r" +
+                                                              "Error :" + ex.Message);
+                    stackTrace = null;
+                }
+                return Clientes;
+            }
+
+        }
+
+        public BusquedaClienteDatosBancariosDatos(IMensajesImplementacion implementadorMensajes)
+            : base(implementadorMensajes)
+        {
+        }
+
+
+        public BusquedaClienteDatosBancariosDatos(List<int> clientes, IMensajesImplementacion implementadorMensajes)
+            : base(clientes, implementadorMensajes)
+        {
+        }
+
+        public override BusquedaClienteDatosBancarios CrearObjeto()
+        {
+            return new BusquedaClienteDatosBancariosDatos(this.implementadorMensajes);
+        }
+
+    }
+}
