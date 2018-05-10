756b71a349fc9699de685c8e145f5f6d8701f895
Ricardo Rojas <rrojas@transformait.com.mx>
2018-05-10 12:18:46 -0500

    Se agregan espacios para forzar la subida de los archivos.

--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/DatosSQL/BusquedaClienteDatosBancariosDatos.cs
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/DatosSQL/BusquedaClienteDatosBancariosDatos.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
+
 namespace Conciliacion.RunTime.DatosSQL
 {
     class BusquedaClienteDatosBancariosDatos : BusquedaClienteDatosBancarios
