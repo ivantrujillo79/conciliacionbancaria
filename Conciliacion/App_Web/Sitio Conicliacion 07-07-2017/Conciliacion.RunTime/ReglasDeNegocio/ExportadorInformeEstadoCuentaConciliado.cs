6880f39dc3197105651db7d20860221a48c7de6f
Iván Trujillo <itrujillo@softgrade.mx>
2018-05-30 17:09:14 -0500

    no message

--- Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/ReglasDeNegocio/ExportadorInformeEstadoCuentaConciliado.cs
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Conciliacion.RunTime/ReglasDeNegocio/ExportadorInformeEstadoCuentaConciliado.cs
@@ -88,7 +88,7 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
         {
             string rutaCompleta = _Ruta + _Archivo;
             ExcelPackage excel = new ExcelPackage(new FileInfo(@rutaCompleta));
-            
+
             return excel;
 
             /*
@@ -148,31 +148,6 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
             empresa = "Corporativo";//_ReporteEstadoCuentaConciliado[0].Corporativo;
             fecha = DateTime.Now;//_ReporteEstadoCuentaConciliado[0].Fecha;
 
-            
-
-
-            /*wsSheet1.Cells["A2"].Value = empresa;
-            wsSheet1.Cells["C3"].Value = fecha.ToString("MMMM", cultureInfo).ToUpper();
-
-            Color colFromHex = System.Drawing.ColorTranslator.FromHtml("#0000FF");
-            wsSheet1.Cells["B1"].Style.Fill.PatternType = ExcelFillStyle.Solid;
-            wsSheet1.Cells["B1"].Style.Fill.BackgroundColor.SetColor(colFromHex);
-            wsSheet1.Cells["B1"].Style.Font.Color.SetColor(Color.White);
-            wsSheet1.Cells["B1"].Value = fecha.ToString("MMMM DE yyyy").ToUpper();
-
-            */
-
-            
-
-            
-
-            /*
-            //banco = _DetalleReporteEstadoCuenta[0].Banco;
-            banco = "BANAMEX ";
-            cuenta = "CTA " + _ReporteEstadoCuentaConciliado[0].CuentaBancoFinanciero + " ";
-            empresa = _ReporteEstadoCuentaConciliado[0].Corporativo;
-            fecha = _ReporteEstadoCuentaConciliado[0].Fecha;*/
-
             // Cuenta
             using (ExcelRange Rng = wsSheet1.Cells["B1:H1"])
             {
@@ -183,7 +158,7 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
             // Empresa
             using (ExcelRange Rng = wsSheet1.Cells["B2:H2"])
             {
-                Rng.Merge= true;
+                Rng.Merge = true;
                 Rng.Value = empresa;
             }
 
@@ -200,10 +175,7 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
                 Rng.Style.Font.Color.SetColor(Color.White);
             }
 
-
-
             // Fecha
-
             using (ExcelRange Rng = wsSheet1.Cells["I1:K3"])
             {
                 Rng.Merge = true;
@@ -213,111 +185,72 @@ namespace Conciliacion.RunTime.ReglasDeNegocio
                 Rng.Style.Fill.BackgroundColor.SetColor(Color.Blue);
                 Rng.Style.Font.Color.SetColor(Color.White);
             }
-            
+
             // Clabe interbancaria
             wsSheet1.Cells["B2"].Value = "CLABE INTERBANCARIA";
 
+            resalteOscuro("Depósito", wsSheet1.Cells["L1"]);            
+            resalteOscuro("Retiro", wsSheet1.Cells["L2"]);            
+            resalteOscuro("Saldo final calculado", wsSheet1.Cells["L3"]);            
+            resalteOscuro("Saldo final bancario", wsSheet1.Cells["L4"]);            
+            resalteOscuro("Depósito conciliado", wsSheet1.Cells["L5"]);            
+            resalteOscuro("Retiro conciliado", wsSheet1.Cells["L6"]);
+            
+            wsSheet1.Protection.IsProtected = false;
+            wsSheet1.Protection.AllowSelectLockedCells = false;
 
             using (ExcelRange Rng = wsSheet1.Cells["B7:P7"])
             {
-                Rng[7, 1].Value = "Fecha";
-                Rng[7, 2].Value = "Referencia";
-                Rng[7, 3].Value = "Concepto";
-                Rng[7, 8].Value = "Retiros";
-                Rng[7, 9].Value = "Depósitos";
-                Rng[7, 10].Value = "Saldo";
-                Rng[7, 11].Value = "Concepto conciliado";
-                Rng[7, 15].Value = "Documento";
-                Rng.Style.Font.Size = 10;
-                Rng.Style.Font.Color.SetColor(Color.Blue);
-                Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                estilarEncabezado("Fecha",Rng[7, 1]);
+                estilarEncabezado("Referencia", Rng[7, 2]);
+                estilarEncabezado("Concepto",Rng[7, 3]);
+                estilarEncabezado("Retiros", Rng["D7:H7"], true);
+                estilarEncabezado("Depósitos",Rng[7, 9]);
+                estilarEncabezado("Saldo", Rng[7, 10]);
+                estilarEncabezado("Concepto conciliado",Rng[7, 11]);
+                estilarEncabezado("Documento", Rng["L7:O7"], true);
 
+                enmarcarRegion(Rng[7, 1, 7, 15]);
             }
 
-            /*            
-                                    // Columnas
-                                    xlRango = xlHoja.Range["B7:P7"];
-                                    xlRango[1, 1].Value2 = "Fecha";
-                                    xlRango[1, 2].Value2 = "Referencia";
-                                    xlRango[1, 3].Value2 = "Concepto";
-                                    xlRango[1, 8].Value2 = "Retiros";
-                                    xlRango[1, 9].Value2 = "Depósitos";
-                                    xlRango[1, 10].Value2 = "Saldo";
-                                    xlRango[1, 11].Value2 = "Concepto conciliado";
-                                    xlRango[1, 15].Value2 = "Documento";
-                                    xlRango.Font.ColorIndex = 23;
-                                    xlRango.Font.Size = 10;
-                                    xlRango.Interior.Color = Excel.XlRgbColor.rgbWhite;
-                                    xlRango.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
-                                    xlRango.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
-                                    xlRango.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlMedium);
-
-                                    xlRango = xlHoja.Columns["A"];
-                                    xlRango.ColumnWidth = 4;
-                                    xlRango.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
-                                    xlRango.Font.Bold = true;
-                                    xlRango = xlHoja.Columns["P"];
-                                    xlRango.ColumnWidth = 13;
-                                    xlRango = xlHoja.Range["D7:H7"];
-                                    xlRango.Merge();
-                                    xlRango = xlHoja.Range["I7:K7"];
-                                    xlRango.Font.Bold = true;
-                                    xlRango.ColumnWidth = 13;
-                                    xlRango = xlHoja.Range["L7:O7"];
-                                    xlRango.Merge();
-                                    xlRango = xlHoja.Range["L7:P7"];
-                                    xlRango.Font.Bold = true;
-                                    xlRango = xlHoja.Range["I:K"];
-                                    xlRango.ColumnWidth = 13;
-                                    xlRango = xlHoja.Range["B:H"];
-                                    xlRango.ColumnWidth = 10;
-                                    */
-
-            using (ExcelRange Rng = wsSheet1.Cells["L1"])
-            {
-                Rng.Value = "Depósito";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            using (ExcelRange Rng = wsSheet1.Cells["L2"])
-            {
-                Rng.Value = "Retiro";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            using (ExcelRange Rng = wsSheet1.Cells["L3"])
-            {
-                Rng.Value = "Saldo final calculado";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            using (ExcelRange Rng = wsSheet1.Cells["L4"])
-            {
-                Rng.Value = "Saldo final bancario";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            using (ExcelRange Rng = wsSheet1.Cells["L5"])
-            {
-                Rng.Value = "Depósito conciliado";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            using (ExcelRange Rng = wsSheet1.Cells["L6"])
-            {
-                Rng.Value = "Retiro conciliado";
-                Rng.Style.Font.Size = 11;
-                Rng.Style.Font.Bold = true;
-                Rng.Style.Font.Italic = true;
-            }
-            wsSheet1.Protection.IsProtected = false;
-            wsSheet1.Protection.AllowSelectLockedCells = false;
             return excelPackage;
+        }
+
+        private void estilarEncabezado(string Texto, ExcelRange Rng)
+        {
+            Rng.Value = Texto;
+            Rng.Style.Font.Size = 10;
+            Rng.Style.Font.Color.SetColor(Color.Blue);
+            Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+        }
+
+        private void estilarEncabezado(string Texto, ExcelRange Rng, Boolean Merge)
+        {
+            Rng.Merge = Merge;
+            Rng.Value = Texto;
+            Rng.Style.Font.Size = 10;
+            Rng.Style.Font.Color.SetColor(Color.Blue);
+            Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+        }
+
+        private void resalteOscuro(string Texto, ExcelRange Rng)
+        {
+            Rng.Value = Texto;
+            Rng.Style.Font.Size = 11;
+            Rng.Style.Font.Bold = true;
+            Rng.Style.Font.Italic = true;
+        }
+
+        private void enmarcarRegion(ExcelRange Rng)
+        {
+            Rng.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+            Rng.Style.Border.Top.Color.SetColor(Color.Black);
+            Rng.Style.Border.Left.Style = ExcelBorderStyle.Medium;
+            Rng.Style.Border.Left.Color.SetColor(Color.Black);
+            Rng.Style.Border.Right.Style = ExcelBorderStyle.Medium;
+            Rng.Style.Border.Right.Color.SetColor(Color.Black);
+            Rng.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+            Rng.Style.Border.Bottom.Color.SetColor(Color.Black);
         }
 
         private void exportarDatos()
