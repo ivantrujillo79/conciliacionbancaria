82475aee77a9c374008597b544f106ff4f22487b
Iván Trujillo <itrujillo@softgrade.mx>
2018-11-30 16:34:36 -0600

    no message

---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/Locker/LockerExterno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locker
+{
+    public static class LockerExterno
+    {
+        public static List<RegistroExternoBloqueado> ExternoBloqueado { get; set; }
+    }
+
+    public class RegistroExternoBloqueado
+    {
+        public string SessionID { get; set; }
+        public int Año { get; set; }
+        public int Folio { get; set; }
+        public int Consecutivo { get; set; }
+    }
+}
---
+++ Conciliacion/App_Web/Sitio Conicliacion 07-07-2017/SitioConciliacion/POCA.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Locker;
+
+public partial class POCA : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LockerExterno.ExternoBloqueado != null)
+        {
+            if (LockerExterno.ExternoBloqueado.Count > 0)
+            {
+                Label1.Text = "Registros: " + LockerExterno.ExternoBloqueado.Count.ToString() + " registro 0: " + LockerExterno.ExternoBloqueado[0].SessionID.ToString() + " " + LockerExterno.ExternoBloqueado[0].Folio.ToString() + " " + LockerExterno.ExternoBloqueado[0].Consecutivo.ToString();
+                foreach (RegistroExternoBloqueado obj in LockerExterno.ExternoBloqueado)
+                {
+                    Response.Write(obj.SessionID.ToString() + " " + obj.Folio.ToString() + " " + obj.Consecutivo.ToString() + "<br/>");
+                }
+            }
+        }
+    }
+}
